9399d106ac6d2e90148c5846d96617c7ca027695
sumit <mazumdarsumit3@gmail.com>
2024-07-09 13:11:59 +0530

    add: about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,8 +2,30 @@ import React from "react";
 
 export default function page() {
   return (
-    <div>
-      <h1>Hi from about</h1>
+    <div className="min-h-screen flex flex-col items-center gap-4 p-5">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to IMDb, your ultimate destination for all things movies,
+        television, and entertainment. As the world’s most comprehensive and
+        authoritative source for movie, TV, and celebrity content, IMDb offers
+        unparalleled access to an extensive database of films, TV shows, actors,
+        directors, and industry professionals.
+      </p>
+      <p>
+        At IMDb, our mission is to empower entertainment fans to explore the
+        world of movies and television. We aim to provide an extensive and
+        accurate database that serves as a go-to resource for discovering,
+        learning, and discussing all aspects of film and television. We are
+        committed to celebrating the art of storytelling and the people behind
+        it.
+      </p>
+      <p>
+        Whether you are a casual viewer, a dedicated film buff, or an industry
+        professional, IMDb is the perfect platform for you. Join our community
+        today and dive into the world of movies and television. Create an
+        account to enjoy personalized recommendations, contribute your reviews,
+        and participate in lively discussions with fellow fans.
+      </p>
     </div>
   );
 }
