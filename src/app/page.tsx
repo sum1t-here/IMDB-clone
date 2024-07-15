790c455400174b91a82dcbd79396b0f3ce28ed29
sumit <mazumdarsumit3@gmail.com>
2024-07-16 02:14:24 +0530

    fetch data from tmdb api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+import Results from "@/components/Results";
 
-export default function page() {
-  return (
-    <div className="min-h-screen">
-      <h1>Hello</h1>
-    </div>
-  );
+const API_KEY = process.env.API_KEY;
+
+interface PageProps {
+  searchParams: {
+    genre?: string;
+  };
+}
+
+export default async function Page({ searchParams }: PageProps) {
+  const genre = searchParams.genre || "fetchTrending";
+  const url = `https://api.themoviedb.org/3${
+    genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+  }?api_key=${API_KEY}&language=en-US&page=1`;
+
+  // Create an AbortController to handle request timeout
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), 10000); // 10 seconds timeout
+
+  try {
+    // Make the fetch request with the AbortController's signal
+    const res = await fetch(url, { signal: controller.signal });
+
+    if (!res.ok) {
+      // Throw an error if the response is not ok
+      throw new Error(`Failed to fetch data: ${res.statusText}`);
+    }
+
+    // Parse the JSON data
+    const data = await res.json();
+    const results = data.results;
+    // console.log(results);
+
+    // Clear the timeout if the fetch request completes successfully
+    clearTimeout(timeoutId);
+
+    return (
+      <div className="min-h-screen">
+        <Results results={results} />
+        {/* Render results if needed */}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+
+    return (
+      <div className="min-h-screen">
+        <h1>Error fetching data</h1>
+      </div>
+    );
+  }
 }
--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -17,7 +17,7 @@ export default function NavbarItem({ title, param }: NavbarItemProps) {
       <Link
         className={`hover:text-amber-600 font-semibold ${
           genre === param
-            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            ? "uxnderline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
             : ""
         }`}
         href={`/?genre=${param}`}
