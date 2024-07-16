626b4280b0b127acd432cccb2e6f25f1f94ee887
sumit <mazumdarsumit3@gmail.com>
2024-07-16 17:14:22 +0530

    use suspense

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
+import React from "react";
 
 interface NavbarItemProps {
   title: string;
@@ -10,14 +11,23 @@ interface NavbarItemProps {
 }
 
 export default function NavbarItem({ title, param }: NavbarItemProps) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NavbarItemContent title={title} param={param} />
+    </Suspense>
+  );
+}
+
+function NavbarItemContent({ title, param }: NavbarItemProps) {
   const searchParam = new URLSearchParams(useSearchParams().toString());
   const genre = searchParam.get("genre");
+
   return (
     <div>
       <Link
         className={`hover:text-amber-600 font-semibold ${
           genre === param
-            ? "uxnderline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
             : ""
         }`}
         href={`/?genre=${param}`}
